53e470a5300cf634fcf959eecfa46a92cd106241
NCordon <nacho.cordon.castillo@gmail.com>
2017-02-23 21:06:41 +0100

    Añade zipeo al archivo de código

--- get-results.sh
+++ get-results.sh
@@ -2,7 +2,9 @@
 
 g++ hash.cc
 
-(./a.out < ./data/me_at_the_zoo.in) > ./data/me_at_the_zoo.out
-(./a.out < ./data/kittens.in) > ./data/kittens.out
-(./a.out < ./data/trending_today.in) > ./data/trending_today.out
-(./a.out < ./data/videos_worth_spreading.in) > ./data/videos_worth_spreading.out
+zip codigo.zip hash.cc
+
+(./a.out < ./data/me_at_the_zoo.in) > ./results/me_at_the_zoo.out
+(./a.out < ./data/kittens.in) > ./results/kittens.out
+(./a.out < ./data/trending_today.in) > ./results/trending_today.out
+(./a.out < ./data/videos_worth_spreading.in) > ./results/videos_worth_spreading.out
